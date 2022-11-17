83bda42e340c81feeab7b71a477d51d864f0afb4
Azibhons <nathanazibabhonomeni@gmail.com>
2022-11-17 01:23:56 +0100

    My shell update

--- shell.h
+++ shell.h
@@ -1,178 +1,62 @@
-#ifndef SHELL_H
-#define SHELL_H
+#ifndef SHELL
+#define SHELL
 
-#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <wait.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <signal.h>
 
 /**
- * struct list_p - singly linked list
- * @ptr: a malloced address
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- * for Holberton shell project
+ * struct list - linked list for environmental variables
+ * @var: holds environmental variable string
+ * @next: points to next node
  */
-typedef struct list_p
+typedef struct list
 {
-	void *ptr;
-	struct list_p *next;
+	char *var;
+	struct list *next;
+
 } list_t;
 
-int linum(int add);
-
-char *get_prog_name(char *name);
-
-void my_error(char *command, int status, char *extra);
-
-void signal_handler(int sig);
-
-void main_loop(char *filename);
-
-/* Below find the linked list functions for use with do_mem*/
-
-size_t __list_len(list_t *h);
-
-list_t *__add_node(list_t **head, void *ptr);
-
-list_t *__add_node_end(list_t **head, void *ptr);
-
-void __free_list(list_t *head);
-
-void __free_list_full(list_t *head);
-
-list_t *__get_node_at_index(list_t *head, unsigned int index);
-
-list_t *__insert_node_at_index(list_t **head, unsigned int idx, void *ptr);
-
-int __delete_node_at_index(list_t **head, unsigned int index);
-
-/* Above find the linked list functions for use with do_mem*/
-
-/**
- * struct list_p2 - singly linked list
- * @ptr: a malloced string
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- * for Holberton shell project
- */
-typedef struct list_p2
-{
-	char *ptr;
-	struct list_p2 *next;
-} list_s;
-
-
-/* Below find the linked list functions */
-
-size_t list_len(list_s *h);
-
-list_s *add_node(list_s **head, char *ptr);
-
-list_s *add_node_end(list_s **head, char *ptr);
-
-void free_list(list_s *head);
-
-void free_list_full(list_s *head);
-
-list_s *get_node_at_index(list_s *head, unsigned int index);
-
-list_s *insert_node_at_index(list_s **head, unsigned int idx, char *ptr);
-
-int delete_node_at_index(list_s **head, unsigned int index);
-
-char **arrayify(list_s *);
-
-list_s *listify(char **);
-
-void free_double_array(char **);
-
-/* Above find the linked list functions */
-
-
-/* Below find the string functions */
-
-int _strcmp(char *s1, char *s2);
-
-int _strlen(char *s);
-
-int word_count(char *str, char *delim);
-
-char *_strcat(char *s1, char *s2);
-
+/* function prototypes */
+int prompt(char **env);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+size_t get_line(char **str);
+int t_strlen(char *str, int pos, char delm);
+char *ignore_space(char *str);
+char **_str_tok(char *str, char *delm);
+char **c_str_tok(char *str, char *delm);
+char *_strcat(char *dest, char *src);
+char *_strdup(char *str);
 char *_strcpy(char *dest, char *src);
+int _strcmp(char *s1, char *s2);
+int _cd(char **str, list_t *env, int num);
+int built_in(char **token, list_t *env, int num, char **command);
+void non_interactive(list_t *env);
+char *_which(char *str, list_t *env);
+int __exit(char **s, list_t *env, int num, char **command);
+int _execve(char *argv[], list_t *env, int num);
+void free_double_ptr(char **str);
+void free_linked_list(list_t *list);
+int _env(char **str, list_t *env);
+char *get_env(char *str, list_t *env);
+list_t *env_linked_list(char **env);
+list_t *add_end_node(list_t **head, char *str);
+size_t print_list(list_t *h);
+int delete_nodeint_at_index(list_t **head, int index);
+int _unsetenv(list_t **env, char **str);
+int _setenv(list_t **env, char **str);
+int find_env(list_t *env, char *str);
+void not_found(char *str, int num, list_t *env);
+void cant_cd_to(char *str, int c_n, list_t *env);
+void illegal_number(char *str, int c_n, list_t *env);
+char *int_to_string(int num);
 
-int _atoi(char *s);
-
-int sizeof_command(char **tokens);
-
-int _isdigit(int c);
-
-int has_newline(char *input);
-
-void shiftbuffer(char *input, int newline_index, int filled);
-
-char *_itoa(int num);
-
-char *_reverse(char *str, int n);
-
-char *_memset(char *s, char b, int n);
-
-/* Above find the string functions */
-
-
-void *do_mem(size_t size, void *ptr);
-
-void do_exit(int fd, char *msg, int code);
-
-ssize_t else_handle_input(char *lineptr, int stream, char *input, int filled);
-
-ssize_t _getline(char *lineptr, int stream);
-
-char **_strtok(char *str, char *delim);
-
-char **get_path();
-
-char *get_env_val(char *name);
-
-char *find_path(char **path, char *command);
-
-char **get_env();
-
-/* environment functions */
-
-char **do_env(char *x, char *y);
-
-char *get_full_command(char *path, char *command);
-
-/* builtin functions */
-
-int setenv_builtin(char **tokens);
-
-int unsetenv_builtin(char **tokens);
-
-int cd_builtin(char **tokens);
-
-char **get_builtins();
-
-int env_builtin(void);
-
-/* execute functions */
-
-int execute(char **tokens);
-
-int exec_nb(char **tokens);
-
-int search_ops(char **tokens);
-
-int exec_builtin(char **tokens, int bcase);
-
-int check_access(char *comm, char *token);
-
-char *prep_execve(char *token);
-
-/* file functions */
-
-char *read_textfile(char *filename);
-
-#endif /*SHELL_H*/
+#endif
